87b774acecbdb51ce0e792dd6f88f8dbcf27bb6c
Jhefferson Muzy <106001033+Jheff8@users.noreply.github.com>
2023-03-05 18:22:22 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ $('div.layout p:last').css('margin-top', '30px').css('margin-bottom', '20px')
 
 $(window).on('load',function(){
     const currentPathName = $(location).prop('pathname')
-    if( currentPathName == 'my_first_web-store/index.html' ) {
+    if( currentPathName == '/my_first_web-store/index.html' ) {
         $('#homeOpt').removeClass('hover').css('border-bottom', '1px solid rgba(25, 29, 38, .9')
-    }else if ( currentPathName == '/my_first_web-store/shop.html' ) {
+    }else if ( currentPathName == '/my first web-store/shop.html' ) {
         $('#shopOpt').removeClass('hover').css('border-bottom', '1px solid rgba(25, 29, 38, .9')
     }else if ( currentPathName == '/my_first_web-store/about.html' ) {
         $('#aboutOpt').removeClass('hover').css('border-botom', '1px solid rgba(25, 29, 38, .9)')
